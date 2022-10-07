0d28666e415290f53dca254f0adb7fe09b9276e8
yingding <yingdingwang@yahoo.de>
2022-10-07 11:50:13 +0200

    added dataclass example

---
+++ standard_lib/data_class_example.py
@@ -0,0 +1,12 @@
+"""
+@dataclass
+"mutable namedtu0ples with default"
+
+https://stackoverflow.com/questions/47955263/what-are-data-classes-and-how-are-they-different-from-common-classes
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()    
