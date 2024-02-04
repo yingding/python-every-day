629494c63c6b6750eee5904fa7a851f5c4b89a2a
yingding <yingdingwang@yahoo.de>
2024-02-04 23:13:20 +0100

    updated multiple dynamic params

--- dynamic_params/func_call.py
+++ dynamic_params/func_call.py
@@ -6,3 +6,10 @@ if __name__ == '__main__':
     foo(1, 2, a=3, b=4, c=5)
     args = {"arg2":2, "a":3, "b":4, "c":5}
     foo(arg1=1, **args)
+    arg_set1 = {"arg2":2}
+    arg_set2 = {"a":3, "b":4, "c":5}
+    foo(arg1=1, **arg_set1, **arg_set2)
+    foo(**arg_set1, **arg_set2, arg1=1)
+    arg_set3 = {"a":3, "arg2":2}
+    arg_set4 = {"b":4, "c":5}
+    foo(**arg_set3, **arg_set4, arg1=1)
